e805c398562a6009fe70993058a7b6956d9f67ec
KIM Miju <76895039+miju0515@users.noreply.github.com>
2024-01-17 17:12:04 +0900

    setting project

---
+++ src/main/java/com/drugbox/DrugBoxApplication.java
@@ -0,0 +1,13 @@
+package com.drugbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrugBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/drugbox/DrugBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drugbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
