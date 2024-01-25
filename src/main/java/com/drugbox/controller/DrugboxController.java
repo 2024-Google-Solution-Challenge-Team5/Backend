e09e3be7d5e2a4b72e3f0da7d0226fd37d3df190
BK <kbomin328@gmail.com>
2024-01-26 02:03:51 +0900

    Style: Map 대신 DTO 클래스 이용해서 Id 반환

--- src/main/java/com/drugbox/controller/DrugboxController.java
+++ src/main/java/com/drugbox/controller/DrugboxController.java
@@ -4,6 +4,7 @@ import com.drugbox.dto.request.DrugboxImageChangeRequest;
 import com.drugbox.dto.request.DrugboxSaveRequest;
 import com.drugbox.dto.response.DrugboxResponse;
 import com.drugbox.dto.response.DrugboxSettingResponse;
+import com.drugbox.dto.response.IdResponse;
 import com.drugbox.service.DrugboxService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,9 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Validated
 @RestController
@@ -29,21 +28,23 @@ public class DrugboxController {
 
     // 구급상자 추가하기 (생성)
     @PostMapping("/add")
-    public ResponseEntity<Map<String, Long>> addDrugbox(@Valid DrugboxSaveRequest request) throws IOException {
+    public ResponseEntity<IdResponse> addDrugbox(@Valid DrugboxSaveRequest request) throws IOException {
         Long drugboxId = drugboxService.addDrugbox(request);
-        Map<String, Long> response = new HashMap<>();
-        response.put("drugboxId", drugboxId);
+        IdResponse response = IdResponse.builder()
+                .id(drugboxId)
+                .build();
         return ResponseEntity.ok(response);
     }
 
 
     // 구급상자 추가하기 (초대)
     @PostMapping("/add/invite-code")
-    public ResponseEntity<Map<String, Long>> addDrugboxByInviteCode(@RequestParam(value="inviteCode") String inviteCode,
+    public ResponseEntity<IdResponse> addDrugboxByInviteCode(@RequestParam(value="inviteCode") String inviteCode,
                                                                     @RequestParam(value="userId") Long userId) throws IOException {
         Long drugboxId = drugboxService.addDrugboxByInviteCode(inviteCode, userId);
-        Map<String, Long> response = new HashMap<>();
-        response.put("drugboxId", drugboxId);
+        IdResponse response = IdResponse.builder()
+                .id(drugboxId)
+                .build();
         return ResponseEntity.ok(response);
     }
 
---
+++ src/main/java/com/drugbox/dto/response/IdResponse.java
@@ -0,0 +1,10 @@
+package com.drugbox.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class IdResponse {
+    private Long id;
+}
