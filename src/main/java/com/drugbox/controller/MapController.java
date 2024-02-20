892d0c42fe2aa75066d4a34c1458cae9a9d87a48
KIM Miju <76895039+miju0515@users.noreply.github.com>
2024-02-20 23:41:46 +0900

    Feat: 지도 위치 검색 및 장소 세부 사항 조회 (#34)
    * Feat: 주변 1km 이내 약국 및 편의점 검색하기
    
    * Feat: 텍스트로 위치 검색하기
    
    * Chore: 빠진 매개변수 추가
    
    * Feat: 장소 세부사항 검색하기
    
    * Chore: API 키 변경
    
    * Chore: 사진 URL 받아오는 방식 수정
    
    * Style: 변수명 변경
    
    * Chore: JSON 파싱 코드 수정

--- src/main/java/com/drugbox/controller/MapController.java
+++ src/main/java/com/drugbox/controller/MapController.java
@@ -1,13 +1,18 @@
 package com.drugbox.controller;
 
+import com.drugbox.dto.request.CoordRequest;
 import com.drugbox.dto.response.BinLocationResponse;
+import com.drugbox.dto.response.MapDetailResponse;
+import com.drugbox.dto.response.MapResponse;
 import com.drugbox.service.MapService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.parser.ParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 
@@ -41,6 +46,25 @@ public class MapController {
             @RequestParam(value="addrLvl1") String addrLvl1,
             @RequestParam(value="addrLvl2", required = false) String addrLvl2){
         List<BinLocationResponse> response = mapService.getDivisionDrugBinLocations(addrLvl1, addrLvl2);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("/nearby")
+    public ResponseEntity<List<MapResponse>> getNearbyPharmacyAndConvenienceLocations(
+            @RequestParam CoordRequest coordRequest) throws IOException, ParseException {
+        List<MapResponse> response = mapService.getNearbyPharmacyAndConvenienceLocations(coordRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<MapResponse>> searchLocations(@RequestParam String name) throws IOException, ParseException {
+        List<MapResponse> responses = mapService.searchLocations(name);
+        return ResponseEntity.status(HttpStatus.OK).body(responses);
+    }
+
+    @GetMapping("/detail")
+    public ResponseEntity<MapDetailResponse> getLocationDetail(@RequestParam String id) throws IOException, ParseException {
+        MapDetailResponse response = mapService.searchLocationDetail(id);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
--- src/main/java/com/drugbox/service/MapService.java
+++ src/main/java/com/drugbox/service/MapService.java
@@ -2,7 +2,10 @@ package com.drugbox.service;
 
 import com.drugbox.common.Util.GeocodingUtil;
 import com.drugbox.domain.BinLocation;
+import com.drugbox.dto.request.CoordRequest;
 import com.drugbox.dto.response.BinLocationResponse;
+import com.drugbox.dto.response.MapDetailResponse;
+import com.drugbox.dto.response.MapResponse;
 import com.drugbox.repository.BinLocationRepository;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
@@ -11,12 +14,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import org.json.simple.parser.JSONParser;
 
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +36,9 @@ import java.util.stream.Collectors;
 public class MapService {
     private final BinLocationRepository binLocationRepository;
     private final GeocodingUtil geocodingUtil;
+
+    @Value("${application.spring.cloud.gcp.placeAPI}")
+    private String API_KEY;
 
     public void saveSeoulDrugBinLocations(){
         JSONParser parser = new JSONParser();
@@ -128,5 +137,140 @@ public class MapService {
                 .addrLvl1(bin.getAddrLvl1())
                 .addrLvl2(bin.getAddrLvl2())
                 .build();
+    }
+
+    public List<MapResponse> getNearbyPharmacyAndConvenienceLocations(CoordRequest coordRequest) throws IOException, ParseException {
+        String requestUrl = "https://places.googleapis.com/v1/places:searchNearby";
+        StringBuilder requestBody = new StringBuilder();
+        requestBody.append("{\"includedTypes\":[\"pharmacy\",\"convenience_store\"],"); // 약국, 편의점 검색
+        requestBody.append("\"locationRestriction\":{" +
+                "\"circle\":{" +
+                    "\"center\":{" +
+                        "\"latitude\":"+ coordRequest.getLatitude() +
+                        ",\"longitude\":" + coordRequest.getLongitude() + "}," +
+                        "\"radius\": 1000.0 } } },");
+        requestBody.append("\"languageCode\":\"ko\"}");
+
+        return getLocationsDetail(requestUrl,requestBody.toString());
+    }
+
+    // 위치 검색하기
+    public List<MapResponse> searchLocations(String name) throws IOException, ParseException {
+        String requestUrl = "https://places.googleapis.com/v1/places:searchText";
+        String requestBody = "{\"textQuery\":\""+ name+"\"," +
+                "\"languageCode\":\"ko\"}";
+
+        return getLocationsDetail(requestUrl,requestBody);
+    }
+
+    // 장소 세부사항
+    public MapDetailResponse searchLocationDetail(String id) throws IOException, ParseException {
+        String requestUrl = "https://places.googleapis.com/v1/places/"+id;
+        return getLocationDetail(requestUrl);
+    }
+
+    private List<MapResponse> getLocationsDetail(String requestUrl, String requestBody) throws IOException, ParseException {
+        URL url = new URL(requestUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type","application/json");
+        conn.setRequestProperty("X-Goog-Api-key",API_KEY);
+        conn.setRequestProperty("X-Goog-FieldMask","places.displayName,places.formattedAddress,places.id");
+
+        try (OutputStream os = conn.getOutputStream()) {
+            byte[] input = requestBody.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+
+        StringBuilder response = new StringBuilder();
+        String responseLine;
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()))){
+            while((responseLine = br.readLine()) != null){
+                response.append(responseLine);
+            }
+        }
+        conn.disconnect();
+
+        return parseLocationsDetail(response.toString());
+    }
+
+    private MapDetailResponse getLocationDetail(String requestUrl) throws IOException, ParseException {
+        URL url = new URL(requestUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-Type","application/json");
+        conn.setRequestProperty("X-Goog-Api-key",API_KEY);
+        conn.setRequestProperty("X-Goog-FieldMask","places.displayName,places.formattedAddress,places.id,places.photos,places.currentOpeningHours");
+
+        StringBuilder response = new StringBuilder();
+        String responseLine;
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()))){
+            while((responseLine = br.readLine()) != null){
+                response.append(responseLine);
+            }
+        }
+        conn.disconnect();
+
+        return parseLocationDetail(response.toString());
+    }
+
+    private List<MapResponse> parseLocationsDetail(String result) throws ParseException {
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) parser.parse(result);
+        JSONArray locations = (JSONArray) jsonObject.get("places");
+        List<MapResponse> mapResponses = new ArrayList<>();
+        for(Object object: locations){
+            JSONObject location = (JSONObject) object;
+            Map<String, String> coords = geocodingUtil.getCoordsByAddress((String)location.get("formattedAddress"));
+            MapResponse mapResponse = MapResponse.builder()
+                    .locationName((String)location.get("displayName"))
+                    .locationAddress((String)location.get("formattedAddress"))
+                    .locationId((String)location.get("id"))
+                    .latitude(coords.get("lat"))
+                    .longitude(coords.get("lng"))
+                    .build();
+            mapResponses.add(mapResponse);
+        }
+        return mapResponses;
+    }
+
+    private MapDetailResponse parseLocationDetail(String result) throws ParseException, IOException {
+        JSONParser parser = new JSONParser();
+        JSONObject location = (JSONObject) parser.parse(result);
+        JSONObject name = (JSONObject) location.get("displayName");
+        JSONArray photos = (JSONArray) location.get("photos");
+        JSONObject photo = (JSONObject) photos.get(0);
+        return MapDetailResponse.builder()
+                .locationName((String)name.get("text"))
+                .locationPhotos(getLocationPhotoUrl((String)photo.get("name")))
+                .locationId((String)location.get("id"))
+                .formattedAddress((String)location.get("formattedAddress"))
+                .currentOpeningHours((String)location.get("currentOpeningHours"))
+                .build();
+    }
+
+    private String getLocationPhotoUrl(String photoName) throws IOException, ParseException {
+        String requestUrl = "https://places.googleapis.com/v1/"+photoName+"/media?key="+API_KEY;
+
+        URL url = new URL(requestUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-Type","application/json");
+
+        StringBuilder response = new StringBuilder();
+        String responseLine;
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()))){
+            while((responseLine = br.readLine()) != null){
+                response.append(responseLine);
+            }
+        }
+        conn.disconnect();
+        return parseLocationPhotoUrl(response.toString());
+    }
+
+    private String parseLocationPhotoUrl(String result) throws ParseException {
+        JSONParser parser = new JSONParser();
+        JSONObject photo = (JSONObject) parser.parse(result);
+        return (String) photo.get("photoUri");
     }
 }
